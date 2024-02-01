277c215bfde1ce20f4b618d969e05803db50ac4a
tessacblack <tessacblack@gmail.com>
2024-02-01 12:34:36 -0700

    1Feb_class_and_Exam_1

--- Exams/Exam_1/Exam1_TCB.R
+++ Exams/Exam_1/Exam1_TCB.R
@@ -6,32 +6,62 @@ df <- read_csv("./data/cleaned_covid_data.csv")
 
 
 # II. Subset the data set to just show states that begin with “A” and save this as an object called A_states. (20 pts)
-# 
+A_states <- df[startsWith(df$Province_State, 'A'),]
+
+
 # Use the tidyverse suite of packages
 # Selecting rows where the state starts with “A” is tricky (you can use the grepl() function or just a vector of those states if you prefer)
 # III. Create a plot of that subset showing Deaths over time, with a separate facet for each state. (20 pts)
-# 
 # Create a scatterplot
 # Add loess curves WITHOUT standard error shading
 # Keep scales “free” in each facet
+ggplot(A_states, aes(x = Last_Update,
+                     y = Deaths))+
+  geom_point()+ # Creates the scatterplot
+  geom_smooth(method = "loess", se = FALSE) +  #adds loess curve without SE shading
+  facet_wrap(~Province_State, scales = "free")
+
+
 # IV. (Back to the full dataset) Find the “peak” of Case_Fatality_Ratio for each state and save this as a new data frame object called state_max_fatality_rate. (20 pts)
-# 
+
 # I’m looking for a new data frame with 2 columns:
 #   
 #   “Province_State”
 # “Maximum_Fatality_Ratio”
 # Arrange the new data frame in descending order by Maximum_Fatality_Ratio
 # This might take a few steps. Be careful about how you deal with missing values!
+
+state_max_fatality_rate <- df %>%
+  group_by(Province_State) %>%
+  summarise(Maximum_Fatality_Ratio = max(Case_Fatality_Ratio, na.rm = TRUE)) %>% 
+  arrange(desc(Maximum_Fatality_Ratio))
+
 #   
 #   V. Use that new data frame from task IV to create another plot. (20 pts)
 # 
-# X-axis is Province_State
-# Y-axis is Maximum_Fatality_Ratio
-# bar plot
-# x-axis arranged in descending order, just like the data frame (make it a factor to accomplish this)
-# X-axis labels turned to 90 deg to be readable
+# X-axis is Province_State ##done
+# Y-axis is Maximum_Fatality_Ratio ##done
+# bar plot ##done
+# x-axis arranged in descending order, just like the data frame (make it a factor to accomplish this). ##done
+# X-axis labels turned to 90 deg to be readable ## done 
 # Even with this partial data set (not current), you should be able to see that (within these dates), different states had very different fatality ratios.
+state_max_fatality_rate %>% 
+  mutate(Province_State = factor(Province_State, levels = Province_State[order(-Maximum_Fatality_Ratio)])) %>% 
+  ggplot(aes(x = reorder(Province_State, -Maximum_Fatality_Ratio),
+             y = Maximum_Fatality_Ratio))+
+  geom_col()+
+  theme(axis.text.x = element_text(angle = 90))
+  labs(x = 'Province_State')
+  
 # 
 # VI. (BONUS 10 pts) Using the FULL data set, plot cumulative deaths for the entire US over time
 # 
 # You’ll need to read ahead a bit and use the dplyr package functions group_by() and summarize() to accomplish this.
+us_cumulative_deaths <- df %>%
+    group_by(Last_Update) %>%
+    summarize(Cumulative_Deaths = sum(Deaths, na.rm = TRUE)) %>%
+    arrange(Last_Update)
+  
+ggplot(us_cumulative_deaths, aes(x = Last_Update, y = Cumulative_Deaths)) +
+    geom_line() +
+    labs(x = "Date", y = "Cumulative_Deaths")
--- Notes/Week_4_Notes.R
+++ Notes/Week_4_Notes.R
@@ -4,8 +4,15 @@
 
 ### Tuesday January 30 and Thursday February 1 ###
 
+### Package of the week ####
+library(leaflet)
+# Lets you create interactive maps
+# R studio has a whole help website that teaches you how to use it 
+
+
 ### Tuesday January 30 ####
 library(tidyverse)
+library(palmerpenguins)
 
 # Convert the following code expressions into "pipe format" to make them more readable
 
@@ -135,6 +142,86 @@ ggplot(penguins, mapping = aes(x = flipper_length_mm,
 library(ggimage)
 
 
+##### Thursday 1 February 2024 ####
+
+# Warm up 
+# make an interesting plot of the penguin data 
+
+## Here's my plot
+str(penguins)
+ggplot(penguins, mapping = aes(x = bill_depth_mm,
+                               y = bill_length_mm))+
+  geom_point(aes(color=species))+
+  geom_smooth(method = "loess", se = TRUE, color = "hotpink")+
+  labs(y = 'Bill Length (mm)', x = 'Bill Depth (mm)')
+
+## This code does the exact same thing, but it's a little easier to read and code
+penguins %>% 
+  ggplot(aes(x = bill_depth_mm,
+             y = bill_length_mm))+
+  geom_point(aes(color=species))+
+  geom_smooth(method = "loess", se = TRUE, color = "hotpink")+
+  labs(y = 'Bill Length (mm)', x = 'Bill Depth (mm)')
+
+  
+penguins %>% 
+  ggplot(aes(x = factor(year), # this splits the plot up to give you three box plots, one per year
+             y = body_mass_g)) + 
+  geom_boxplot()+
+  geom_point() # all of the points are overlapping and hiding the data
+
+penguins %>% 
+  ggplot(aes(x = factor(year),
+             y = body_mass_g)) + 
+  geom_boxplot()+
+  geom_jitter(height=0,width=0.1,alpha=0.2) # avoids overplotting issue by introducing random variation 
+# Boxplots tend to hide the data, so this shows where the data actually lies 
+# Boxplots really want one numeric and one categorical variable. Not two cat or two num
 
 
-#
+penguins %>% 
+  ggplot(aes(x=body_mass_g, fill=species))+
+  geom_density(alpha=.25)
+# This created a distribution. Always a good idea to look at the distribution of the variables of interest 
+## Dr. Z's rules for plottiing
+# Rule 1: Don't hide your data
+# Rule 2: Have a goal for your plot. What are you trying to discover from your data that plotting would help with? 
+
+
+df <- read_delim("./Data/DatasaurusDozen.tsv")
+df %>% 
+  group_by(dataset) %>% # Why would you want to group a dataframe by some data? Bc you want to do groupby summarize
+  summarize(meanx=mean(x),
+            sdx=sd(x),
+            minx=min(x),
+            medianx=mean(x)) # The means and sd are exactly the same. The median and min are roughtly the same 
+df %>% 
+  ggplot(aes(x=x,fill=dataset))+
+  geom_density(alpha=0.3) 
+# Even though the summary statistics are exactly the same, the distributions are wildly different
+
+df %>% 
+  ggplot(aes(x=x,y=y))+
+  geom_point()+
+  facet_wrap(~dataset) 
+# facet syntax: ~Columnname. Creates different subplots for each column
+#See, a t-test would tell you there is no different in these means
+
+
+library(GGally)
+ggpairs(penguins) # Look at all of the variables in your entire dataset. Works best when you have less than ten variables 
+# Takes every possible combo of all of our columns and plots them. 
+# It also gives you correlations and an automatic significance value for those correlations 
+
+penguins %>% 
+  ggplot(aes(x=body_mass_g,
+             y=bill_depth_mm))+
+  geom_point(aes(color=sex),shape=18,size=4,alpha=0.75)+
+  facet_wrap(~species,)+
+  scale_color_manual(values=c("rosybrown1","steelblue4"))
+
+# If you set the scale to free, it's harder to see the differences between the datasets. This has hidden the data a little bit
+# Rule X: Make it easy for people to see the story in your figure 
+
+
+# 
