9d45a7c3cc95670a202fe979215a2c19c9d46724
tessacblack <tessacblack@gmail.com>
2024-03-02 13:21:48 -0700

    Exam2_March3

--- Exams/Exam_2/BLACK_Exam2_Script.R
+++ Exams/Exam_2/BLACK_Exam2_Script.R
@@ -16,34 +16,34 @@ unicef <- janitor::clean_names(unicef)
 names(unicef)
 
 # Columns: country, u5mr, year. remove "u5mr_" from year column
-clean <- unicef %>% 
+unicef <- unicef %>% 
   pivot_longer(starts_with("u5mr_"),
                names_to = "year",
                values_to = "u5mr") %>% 
   rename(country = country_name) %>% 
-  mutate(year = str_remove(year, "u5mr_"))
+  mutate(year = str_remove(year, "u5mr_")) %>% 
+  mutate(year = as.numeric(year))
 
 
 # Task 3: Plot each country’s U5MR over time (20 points) ####
 # Create a line plot (not a smooth trend line) for each country
 # Facet by continent
 plot1 <- 
-  clean %>% 
-  ggplot(aes(x = year, y = u5mr, group = country))+
-  geom_line()+
+  unicef %>% 
+  ggplot(aes(x = year, y = u5mr))+
+  geom_path()+
   facet_wrap(~continent)+
-  scale_x_discrete(breaks = c(1960, 1980, 2000))+
-  theme_minimal()
+  theme_bw()
 print(plot1)  
 
 # Task 4: Save this plot as LASTNAME_Plot_1.png (5 pts) ####
-#ggsave("./BLACK_Plot_1.png", plot = plot1)
+ggsave("./BLACK_Plot_1.png", plot = plot1)
 
 # Task 5: Create another plot that shows the mean U5MR for all the countries within a given continent at each year (20 pts) ####
 # Another line plot (not smooth trendline)
 # Colored by continent
 
-task5 <- clean %>% 
+task5 <- unicef %>% 
   group_by(year, continent) %>% 
   summarize(meanu5mr = mean(u5mr, na.rm = TRUE))
 
@@ -52,44 +52,47 @@ plot2 <- task5 %>%
              color = continent,
              group = continent))+
   geom_line(linewidth= 1.5)+
-  scale_x_discrete(breaks = c(1960, 1980, 2000))+
-  theme_minimal()
+  theme_bw()
 print(plot2)
 
 
 # Task 6: Save that plot as LASTNAME_Plot_2.png (5 pts) ####
-#ggsave("./BLACK_Plot_2.png", plot = plot2)
+ggsave("./BLACK_Plot_2.png", plot = plot2)
 
 
 # Task 7: Create three models of U5MR (20 pts) ####
 # mod1 should account for only Year
 # mod2 should account for Year and Continent
 # mod3 should account for Year, Continent, and their interaction term
-mod1 <- glm(data = clean, formula = u5mr ~ year)
-mod2 <- glm(data = clean, formula = u5mr ~ year + continent)
-mod3 <- glm(data = clean, formula = u5mr ~ year * continent)
+mod1 <- glm(data = unicef, formula = u5mr ~ year)
 summary(mod1)
+
+mod2 <- glm(data = unicef, formula = u5mr ~ year + continent)
+summary(mod2)
+
+mod3 <- glm(data = unicef, formula = u5mr ~ year * continent)
+summary(mod3)
 
 # Task 8: Compare the three models with respect to their performance ####
 # Your code should do the comparing
 # Include a comment line explaining which of these three models you think is best
-compare_models(mod1, mod2, mod3)
+compare_performance(mod1, mod2, mod3) 
 compare_performance(mod1, mod2, mod3) %>% plot
-# I think mod3 is the best model. It has a higher R2 and RMSE
+# I think mod3 is the best model. It has a higher R2 and RMSE. 
 
 
 # Task 9: Plot the 3 models’ predictions like so: (10 pts) ####
-clean$pred1 <- predict(mod1, clean)
-clean$pred2 <- predict(mod2, clean)
-clean$pred3 <- predict(mod3, clean)
-clean <- clean %>% 
-  pivot_longer(starts_with("pred"), names_to = "model", values_to = "prediction")
-
-clean %>% 
-  ggplot(aes(x = year, y = prediction, color = continent))+
-  geom_line()+
+unicef$mod1 <- predict(mod1, unicef)
+unicef$mod2 <- predict(mod2, unicef)
+unicef$mod3 <- predict(mod3, unicef)
+unicef %>% 
+  pivot_longer(starts_with("mod"),
+               names_to = "model",
+               values_to = "Predicted_U5MR") %>% 
+  ggplot(aes(x = year, y = Predicted_U5MR, color = continent))+
+  geom_smooth(method = 'lm',se=FALSE)+
   facet_wrap(~model)+
-  theme_minimal()
+  theme_bw()
 
 
 # Task 10: BONUS - Using your preferred model, predict what the U5MR would be for 
